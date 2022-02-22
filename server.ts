cd0399c7f3d64655c145611cf7bfa55966ebf131
Felipe Barcelos <felipebarcelospro@iMac-de-Felipe.local>
2022-02-22 18:57:22 -0300

    first commit

---
+++ server.ts
@@ -0,0 +1,62 @@
+import { PrismaClient } from '@prisma/client';
+import express from 'express';
+
+const app = express();
+const prisma = new PrismaClient()
+
+app.use(express.json());
+
+app.get('/users', async (req, res) => {
+  const users = await prisma.user.findMany()
+
+  res.send(users)
+});
+
+app.get('/users/:id', async (req, res) => {
+  const user = await prisma.user.findUnique({
+    where: {
+      id: req.params.id
+    }
+  })
+
+  res.send(user)
+});
+
+app.post('/users', async (req, res) => {
+  const user = await prisma.user.create({
+    data: {
+      name: req.body.name,
+      email: req.body.email
+    }
+  })
+
+  res.send(user)
+});
+
+app.put('/users/:id', async (req, res) => {
+  const user = await prisma.user.update({
+    where: {
+      id: req.params.id
+    },
+    data: {
+      name: req.body.name,
+      email: req.body.email
+    }
+  })
+
+  res.send(user)
+});
+
+app.delete('/users/:id', async (req, res) => {
+  await prisma.user.delete({
+    where: {
+      id: req.params.id
+    }
+  })
+
+  res.send()
+});
+
+app.listen(3333, () => {
+  console.log('Server started on port 3333!');
+})
