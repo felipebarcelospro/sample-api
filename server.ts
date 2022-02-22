ece6ee6dd97cb7c51f7e1877450fd13bad4461b7
Felipe Barcelos <felipebarcelospro@iMac-de-Felipe.local>
2022-02-22 19:16:16 -0300

    feat: config ports

--- server.ts
+++ server.ts
@@ -57,6 +57,7 @@ app.delete('/users/:id', async (req, res) => {
   res.send()
 });
 
-app.listen(3333, () => {
-  console.log('Server started on port 3333!');
+const port = process.env.PORT;
+app.listen(port, () => {
+  console.log(`Server started on port ${port}!`);
 })
